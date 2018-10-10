afe8fd0146f25edb3b92538a4e925af7a1b8231e
Zyrene Adao <maryzyrene.adao@uconn.edu>
2018-10-09 22:13:34 -0400

    fixed tests

--- tests.py
+++ tests.py
@@ -1,9 +1,7 @@
 import pytest
 
-
 class TestApp:
-
 	def test_index_loads(self, client):
-		response = client.get(url_for('index'))
+		response = client.get('/index')
 		assert response.status_code == 200
 
