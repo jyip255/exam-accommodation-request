d88ae9e3cd8bb30ec8b91234f97d7379d8c11a60
Zyrene Adao <maryzyrene.adao@uconn.edu>
2018-10-09 23:01:28 -0400

    changed type

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class Demo3(db.Model):
     sid = db.Column(db.Integer, primary_key=True)
     sname = db.Column(db.String(120))
-    netid = db.Column(db.Integer)
+    netid = db.Column(db.String(120))
     cid = db.Column(db.String(120), primary_key=True)
     cname = db.Column(db.String(120))
     # Defines how the object prints in console
