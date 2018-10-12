7bf8c081ab7fd1aaca0c98922a3a9fe20edf2c4b
Jason Yip <jason.yip@uconn.edu>
2018-10-12 16:20:49 -0400

    Added exam request info, functional index, search, and request list

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,12 @@ from wtforms.validators import DataRequired
 class StudentForm(FlaskForm):
     searchStudents = StringField('', validators=[DataRequired()])
     submit = SubmitField('Search Students by Name')
+
+# Form for adding request to the database
+class AddForm(FlaskForm):
+    sname = StringField('', validators=[DataRequired()])
+    sid = StringField('', validators=[DataRequired()])
+    netid = StringField('', validators=[DataRequired()])
+    cid = StringField('', validators=[DataRequired()])
+    cname = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from app import app, db
 
-from app.forms import StudentForm
+from app.forms import StudentForm, AddForm
 from app.models import Demo3
 
 @app.route('/', methods=['GET', 'POST'])
@@ -29,4 +29,36 @@ def index():
         return render_template('index.html', form=form, results=searchResults)
     # If form is not validated or is making GET request (not POST), return index.html without results
     return render_template('index.html', form=form)
+
+@app.route('/requestAdd', methods=['GET','POST'])
+def add():
+    # Get AddForm from forms.py
+    form = AddForm(request.form)
+    if form.validate_on_submit():
+        # Input for the information
+        sidQuery = Demo3.query.filter_by(sid=form.sid.data).all()
+        snameQuery = Demo3.query.filter_by(sname=form.sname.data).all()
+        netidQuery = Demo3.query.filter_by(netid=form.netid.data).all()
+        cidQuery = Demo3.query.filter_by(cid=form.cid.data).all()
+        cnameQuery = Demo3.query.filter_by(cname=form.cname.data).all()
+        #if (sidQuery == []) or (snameQuery == []):
+            # return render_template('requestAdd.html', form=form, msg="Invalid track or playlist name.")
+        # If the input is valid, add the new request to the database
+        #else:
+        p = Demo3(sid=sidQuery[0].sid, sname=snameQuery[0].sname, netid=netidQuery[0].netid, cid=cidQuery[0].cid, cname=cnameQuery[0].cname)
+        db.session.add(p)
+        db.session.commit()
+        return render_template('requestAdd.html', form=form, msg="Track added!")
+    return render_template('requestAdd.html', form=form)
+
+@app.route('/requestList')
+def requestList():
+    # Get all records in Demo3 table (can look up SQLAlchemy commands to filter results, uses Demo3 in models.py)
+    students = Demo3.query.all()
+    # For each student
+    for student in students:
+        # Display all info from DB (see console)
+        print('<SID:{}, SNAME:{}, NETID:{}, CID:{}, CNAME:{}>'.format(student.sid, student.sname, student.netid, student.cid, student.cname))
+    # Send information to requestList.html to be shown on screen
+    return render_template('requestList.html', rows = students)
 
