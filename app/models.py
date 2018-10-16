396365b951113f62b4282230f7e8fb3b86e59631
John Henares <john.henares@uconn.edu>
2018-10-16 02:13:32 -0400

    table now contains all exam request data

--- app/models.py
+++ app/models.py
@@ -2,11 +2,26 @@ from app import db
 
 # Class/attributes should reflect table name and table attributes
 class Demo3(db.Model):
-    sid = db.Column(db.Integer, primary_key=True)
-    sname = db.Column(db.String(120))
-    netid = db.Column(db.String(120))
-    cid = db.Column(db.String(120), primary_key=True)
-    cname = db.Column(db.String(120))
+    student_id = db.Column(db.Integer, primary_key=True)
+    student_netid = db.Column(db.String(120))
+    student_name = db.Column(db.String(120))
+    course_id = db.Column(db.String(120))
+    course_name = db.Column(db.String(120))
+    exam_type = db.Column(db.String(120))
+    exam_format = db.Column(db.String(120))
+    exam_time = db.Column(db.String(120))
+    exam_csd_time = db.Column(db.String(120))
+    csd_campus = db.Column(db.String(120))
+    csd_building = db.Column(db.String(120))
+    csd_room = db.Column(db.String(120))
+    csd_seat = db.Column(db.String(120))
+    instructor_netid = db.Column(db.String(120))
+    instructor_name = db.Column(db.String(120))
+    instructor_phone = db.Column(db.String(120))
+    instructor_email = db.Column(db.String(120))
+    material = db.Column(db.String(120))
+    accommodations = db.Column(db.String(120))
+    instructor_notes = db.Column(db.String(120))
     # Defines how the object prints in console
     def __repr__(self):
-        return '<Record for {}>'.format(self.sname)
+        return '<Record for {}>'.format(self.student_name)
--- app/views.py
+++ app/views.py
@@ -20,9 +20,10 @@ def index():
         # For each student
         for student in students:
             # Display all info from DB (see console)
-            print('<SID:{}, SNAME:{}, NETID:{}, CID:{}, CNAME:{}>'.format(student.sid, student.sname, student.netid, student.cid, student.cname))
+            print('<STUDENT_ID:{}, STUDENT_NETID:{}, STUDENT_NAME:{}, COURSE_ID:{}, COURSE_NAME:{}, EXAM_TYPE:{}, EXAM_FORMAT:{}, EXAM_TIME:{}, EXAM_CSD_TIME:{}, CSD_CAMPUS:{}, CSD_BUILDING:{}, CSD_ROOM:{}, CSD_SEAT:{}, INSTRUCTOR_NETID:{}, INSTRUCTOR_NAME:{}, INSTRUCTOR_PHONE:{}, INSTRUCTOR_EMAIL:{}, MATERIAL:{}, ACCOMMODATIONS:{}, INSTRUCTOR_NOTES:{}>'
+                .format(student.student_id, student.student_netid, student.student_name,  student.course_id, student.course_name, student.exam_type, student.exam_format, student.exam_time, student.exam_csd_time, student.csd_campus, student.csd_building, student.csd_room, student.csd_seat, student.instructor_netid, student.instructor_name, student.instructor_phone, student.instructor_email, student.material, student.accommodations, student.instructor_notes))
             # Check if student name matches form input
-            if student.sname == form.searchStudents.data:
+            if student.student_name == form.searchStudents.data:
                 # If so, add student to search results
                 searchResults.append(student)
             # Send information to index.html to be shown on screen
@@ -36,7 +37,26 @@ def add():
     form = AddForm(request.form)
     if form.validate_on_submit():
         # Add the new request to the database
-        p = Demo3(sid=form.sid.data, sname=form.sname.data, netid=form.netid.data, cid=form.cid.data, cname=form.cname.data)
+        p = Demo3(student_id=form.student_id.data,
+            student_netid=form.student_netid.data,
+            student_name=form.student_name.data, 
+            course_id=form.course_id.data, 
+            course_name=form.course_name.data,
+            exam_type=form.exam_type.data,
+            exam_format=form.exam_format.data,
+            exam_time=form.exam_time.data,
+            exam_csd_time=form.exam_csd_time.data,
+            csd_campus=form.csd_campus.data,
+            csd_building=form.csd_building.data,
+            csd_room=form.csd_room.data,
+            csd_seat=form.csd_seat.data,
+            instructor_netid=form.instructor_netid.data,
+            instructor_name=form.instructor_name.data,
+            instructor_phone=form.instructor_phone.data,
+            instructor_email=form.instructor_email.data,
+            material=form.material.data,
+            accommodations=form.accommodations.data,
+            instructor_notes=form.instructor_notes.data)
         db.session.add(p)
         db.session.commit()
         return render_template('requestAdd.html', form=form, msg="Exam request added!")
