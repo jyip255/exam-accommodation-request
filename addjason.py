7f97e7077adb1e349cca1fff2c55dcb72442edaf
Zyrene Adao <maryzyrene.adao@uconn.edu>
2018-11-29 14:41:44 -0500

    added dependencies to requirements.txt, cleaned up files (didnt remove db)

--- addjason.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('exam_requests.db')
-
-conn.execute("ALTER TABLE examrequest ADD COLUMN FILE_PATH")
-
-conn.commit()
-conn.close()
--- qrtest/qrread.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-import pyzbar.pyzbar as pyzbar
-
-filename = 'mycode'
-decodedImg = pyzbar.decode(Image.open("./static/"+filename+".png"))[0].data.decode("utf-8")
-print(decodedImg)
